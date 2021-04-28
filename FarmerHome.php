04d2c5301c7ef5529139ecdfb110bb3985c29be1
Logan <athuldineshan5@gmail.com>
2021-04-28 08:58:40 +0530

    FarmerHome: Tab Functionality

--- FarmerHome.php
+++ FarmerHome.php
@@ -5,7 +5,23 @@ $fid=$_SESSION['fid'];
 $sql="select * from farmer where fid=$fid";
 $farmer=mysqli_query($con,$sql);
 $data=mysqli_fetch_assoc($farmer);
+
+if(isset($_POST['accept'])){
+    $auid=$_POST['auid'];
+    echo $auid;
+    $query="UPDATE auction SET status=1 WHERE auid = $auid";
+    $result=mysqli_query($con,$query);
+     header("Location: FarmerHome.php");
+}
+elseif(isset($_POST['cancel'])){
+    $auid=$_POST['auid'];
+    $query="DELETE from auction  where auid = $auid";
+    $result=mysqli_query($con,$query);
+     header("Location: FarmerHome.php");
+}
+
 ?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -54,21 +70,112 @@ $data=mysqli_fetch_assoc($farmer);
         <label for="tab2">Completed Auctions</label>
 
         <section id="content1">
-            <div class="active-auct">
-                <img src="./assets/wheat.png" class="auct-img">
-                <ul class="auct-items">
-                    <li> Crop: <span style="font-weight: 600;">Wheat (10kg)</span></li>
-                    <li>Time Remaining: <span style="font-weight: 600;">01:00:00</span></li>
-                </ul>
-                <div class="bid-items">
-                    <li>Highest Bid: ₹ 15,000</li>
-                    <li>By: Athul Dinesan</li>
-                </div>
-                <div class="cancel-btn"><a href="#">Cancel</a></div>
+            <?php
+            $sql2="SELECT * FROM auction WHERE status=0 AND fid=$fid ORDER BY auid DESC";
+            $records=mysqli_query($con,$sql2);
+            while($data = mysqli_fetch_array($records))
+            {
+               $retailerId=$data['rid'];
+               $selectSql="SELECT rname FROM retailer WHERE rid=$retailerId";
+               $selectResult=mysqli_query($con,$selectSql);
+               $selectData=mysqli_fetch_assoc($selectResult);
+              $rname=$selectData['rname'];            
+            ?>
+
+        <div class="active-auct">
+            <?php 
+
+                if($data['crop']=="Wheat"){
+                echo"<img src='./assets/wheat.png' class='auct-img'>"; 
+                }
+                else if($data['crop']=="Coconut"){
+                echo"<img src='./assets/coconut.png' class='auct-img'>";
+                }
+                else{
+                echo"<img src='./assets/rice.png' class='auct-img'>";
+                }
+            ?>
+            <ul class="auct-items">
+                <li> Crop: <span style="font-weight: 600;"><?php echo $data['crop'] . " ( " . $data['quantity'] . "kg )"?></span></li>
+                <li> Base Prize:  <span style="font-weight: 600;">₹ <?php echo $data['baseprize']?></li>  
+                <li> <span style="font-size: small;"><?php echo "Description: " . $data['description']?></span></li>
+            </ul>
+            <div class="bid-items">
+                <li>Highest Bid: <span style="color: #94ee92;font-weight: 600;">₹ <?php echo $data['currprize'] ?></span></li>
+                <?php 
+                if($data['rid']==0){
+                echo "<li>By: <span style='font-weight: 600;'>No Bids</span></li>";
+                }
+                else
+                {
+
+                echo "<li>By: <span style='font-weight: 600;'>$rname</span></li>";
+                }
+                ?>
             </div>
+            <form method="post">
+            <input type="hidden" name="auid" value="<?php echo $data['auid']?>">
+            <button class="accept-btn" type="submit"  name="accept">Wind up</button>
+            <button class="cancel-btn"  type="submit" name="cancel"> Cancel</button>
+            </form>
+         </div>
+        <?php
+            }
+            ?>
         </section>
 
         <section id="content2">
+            <?php
+            $sql2="SELECT * FROM auction WHERE status=1 AND fid=$fid ORDER BY auid DESC";
+            $records=mysqli_query($con,$sql2);
+            while($data = mysqli_fetch_array($records))
+            {
+               $retailerId=$data['rid'];
+               $selectSql="SELECT rname,phone FROM retailer WHERE rid=$retailerId";
+               $selectResult=mysqli_query($con,$selectSql);
+               $selectData=mysqli_fetch_assoc($selectResult);
+              $rname=$selectData['rname'];
+			  $phone=$selectData['phone'];			  
+            ?>
+
+        <div class="active-auct">
+            <?php 
+
+                if($data['crop']=="Wheat"){
+                echo"<img src='./assets/wheat.png' class='auct-img'>"; 
+                }
+                else if($data['crop']=="Coconut"){
+                echo"<img src='./assets/coconut.png' class='auct-img'>";
+                }
+                else{
+                echo"<img src='./assets/rice.png' class='auct-img'>";
+                }
+            ?>
+            <ul class="auct-items">
+                <li> Crop: <span style="font-weight: 600;"><?php echo $data['crop'] . " ( " . $data['quantity'] . "kg )"?></span></li>
+                <li> Base Prize:  <span style="font-weight: 600;">₹ <?php echo $data['baseprize']?></li>  
+                <li> <span style="font-size: small;"><?php echo "Description: " . $data['description']?></span></li>
+            </ul>
+            <div class="bid-items">
+                <li>Highest Bid: <span style="color: #94ee92;font-weight: 600;">₹ <?php echo $data['currprize'] ?></span></li>
+                <?php 
+                if($data['rid']==0){
+                echo "<li>By: <span style='font-weight: 600;'>No Bids</span></li>";
+                }
+                else
+                {
+
+                echo "<li>By: <span style='font-weight: 600;'>$rname</span></li>";
+                }
+				echo "<li>Phone: <span style='font-weight: 600;'>$phone</span></li>";
+                ?>
+            </div>
+              <button class="accept-btn" type="submit" >Completed</button>
+         </div>
+        <?php
+            }
+            ?>
+
 
         </section>
 
